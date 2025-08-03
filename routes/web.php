252f94f6c046a5463815cdb808723c8dfff9b641
Mohammad Ali Abdullah <maabdullah511@gmail.com>
2025-08-04 05:40:04 +0600

    remove app.js and add add web route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\SaleController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TrashController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,5 +22,16 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('products', ProductController::class);
+Route::resource('customers', CustomerController::class);
+Route::get('/sales', [SaleController::class, 'index'])->name('sales.index');
+Route::post('/sales', [SaleController::class, 'store'])->name('sales.store');
+Route::get('/sales/create', [SaleController::class, 'create'])->name('sales.create');
+Route::get('/sales/{sale}', [SaleController::class, 'show'])->name('sales.show');
+Route::delete('/sales/{sale}', [SaleController::class, 'destroy'])->name('sales.destroy');
+Route::post('/notes', [NoteController::class, 'store'])->name('notes.store');
+Route::get('/trash', [TrashController::class, 'index'])->name('trash.index');
+Route::get('/trash/{id}/restore', [TrashController::class, 'restore'])->name('trash.restore');
 
 require __DIR__.'/auth.php';
